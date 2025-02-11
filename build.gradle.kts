19958d281e739402dc2361a49826c3f21a0a6e16
murilommen <murilommen@gmail.com>
2025-02-11 13:30:38 -0300

    fixes for micronaut startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     id("java")
+    id("application")
+    id("io.micronaut.application") version "4.2.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
 
 repositories {
     mavenCentral()
@@ -11,13 +15,15 @@ repositories {
 }
 
 dependencies {
+    // Tests w/ JUnit
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    // Micronaut dependencies (with explicit versions)
+    // Micronaut dependencies
     implementation("io.micronaut:micronaut-runtime:4.2.0")
     implementation("io.micronaut.data:micronaut-data-hibernate-jpa:4.2.0")
-    implementation("io.micronaut.flyway:micronaut-flyway:4.2.0")
+    implementation("io.micronaut.openapi:micronaut-openapi")
+    implementation("io.micronaut:micronaut-jackson-databind")
 
     // Jakarta Persistence API
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
@@ -26,17 +32,39 @@ dependencies {
 
     // Database dependencies
     implementation("org.postgresql:postgresql:42.7.2")
-    implementation("org.flywaydb:flyway-core:9.22.3")
 
-    // Lombok for reducing boilerplate
+    // Lombok - reduce boilerplate code
     implementation("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
-//
-//    // Logging
-//    runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
+
+    runtimeOnly("org.yaml:snakeyaml:2.2")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
 }
 
+micronaut {
+    version("4.2.0")
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("*")
+    }
+}
+
+//micronaut {
+//    runtime("netty") // Default Micronaut runtime
+//    testRuntime("junit5")
+//    processing {
+//        incremental(true)
+//        annotations("org.example.*")
+//    }
+//}
+
+application {
+    mainClass.set("com.wallet.Application")
+}
 
 tasks.test {
     useJUnitPlatform()
 }
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "wallet-backend"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/releases") }
+    }
+}
